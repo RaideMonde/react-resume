2cc7bd0136e2b4133f5143504c992e5e9fa53640
Rahul Singh <i.am.rahul@live.com>
2024-10-29 17:14:59 -0700

    Added getCurrentDate

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -41,6 +41,18 @@ import {
 } from './dataDef';
 
 /**
+ * Date function
+ */
+export function getCurrentDate(separator=''){
+  let newDate = new Date()
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
+
+  return `${year}${separator}${month<10?`0${month}`:`$month}`}${separator}${date}`
+}
+
+/**
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
